0f4ca9218c4b9fd134b6bb5f3411a4f13882a815
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-11-23 16:59:15 +0100

    Add support for special field types in strategy

--- src/scitacean/testing/strategies.py
+++ src/scitacean/testing/strategies.py
@@ -4,13 +4,62 @@
 from functools import partial
 from typing import Optional
 
+from email_validator import validate_email, EmailNotValidError
 from scitacean import Dataset, DatasetType, PID
+from scitacean._internal.orcid import orcid_checksum
 from hypothesis import strategies as st
+
+
+# email_validator and by extension pydantic is more picky than hypothesis
+# so make sure that generated emails actually pass model validation.
+def _is_valid_email(email: str) -> bool:
+    try:
+        validate_email(email, check_deliverability=False)
+        return True
+    except EmailNotValidError:
+        return False
+
+
+def _email_field_strategy(
+    field: Dataset.Field, dataset_type: DatasetType
+) -> st.SearchStrategy:
+    if field.required(dataset_type):
+        return st.emails().filter(_is_valid_email)
+    return st.none() | st.emails().filter(_is_valid_email)
+
+
+def _orcid_field_strategy(
+    field: Dataset.Field, dataset_type: DatasetType
+) -> st.SearchStrategy:
+    def make_orcid(digits: str):
+        digits = digits[:-1] + orcid_checksum(digits)
+        return "https://orcid.org/" + "-".join(
+            digits[i : i + 4] for i in range(0, 16, 4)
+        )
+
+    orcid_strategy = st.text(alphabet="0123456789", min_size=16, max_size=16).map(
+        make_orcid
+    )
+
+    if field.required(dataset_type):
+        return orcid_strategy
+    return st.none() | orcid_strategy
+
+
+_SPECIAL_FIELDS = {
+    "investigator": _email_field_strategy,
+    "contact_email": _email_field_strategy,
+    "owner_email": _email_field_strategy,
+    "orcid_of_owner": _orcid_field_strategy,
+}
 
 
 def _field_strategy(
     field: Dataset.Field, dataset_type: DatasetType
 ) -> st.SearchStrategy:
+    if (strategy := _SPECIAL_FIELDS.get(field.name)) is not None:
+        return strategy(field, dataset_type)
+
     return st.from_type(
         field.type if field.required(dataset_type) else Optional[field.type]
     )
@@ -43,5 +92,5 @@ def datasets(dataset_type: Optional[DatasetType] = None, **fixed) -> st.SearchSt
         type=st.just(dataset_type),
         _read_only=read_only_arg,
         _pid=st.from_type(PID),
-        **kwargs
+        **kwargs,
     )
---
+++ tests/testing/strategies_test.py
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scitacean contributors (https://github.com/SciCatProject/scitacean)
+# @author Jan-Lukas Wynen
+from dateutil.parser import parse as parse_datetime
+from hypothesis import given, settings, strategies as st
+from scitacean import DatasetType
+from scitacean.testing import strategies as sst
+
+
+@given(sst.datasets())
+def test_datasets_makes_valid_dataset(dset):
+    _ = dset.make_models()
+
+
+@settings(max_examples=10)
+@given(sst.datasets(dataset_type=DatasetType.RAW))
+def test_datasets_can_set_type_to_raw(dset):
+    assert dset.type == "raw"
+
+
+@settings(max_examples=10)
+@given(sst.datasets(dataset_type=DatasetType.DERIVED))
+def test_datasets_can_set_type_to_derived(dset):
+    assert dset.type == "derived"
+
+
+@settings(max_examples=20)
+@given(
+    sst.datasets(
+        name="my-dataset-name",
+        access_groups=["group1", "another-group"],
+        source_folder="/the/source/of/truth",
+        created_at=parse_datetime("2123-11-29T17:44:56Z"),
+    )
+)
+def test_datasets_can_fix_fields(dset):
+    assert dset.name == "my-dataset-name"
+    assert dset.access_groups == ["group1", "another-group"]
+    assert dset.source_folder == "/the/source/of/truth"
+    assert dset.created_at == parse_datetime("2123-11-29T17:44:56Z")
+
+
+@settings(max_examples=20)
+@given(
+    sst.datasets(
+        created_by=st.just("the-creator"), owner=st.sampled_from(("owner1", "owner2"))
+    )
+)
+def test_datasets_can_set_strategy_for_fields(dset):
+    assert dset.created_by == "the-creator"
+    assert dset.owner in ("owner1", "owner2")
