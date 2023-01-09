df4fb5b2a463b00984a3090251d7bcf66efefdbd
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-01-09 10:17:19 +0100

    Add logging.logger_name

--- src/scitacean/logging.py
+++ src/scitacean/logging.py
@@ -1,8 +1,20 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/scitacean)
+"""Utilities for logging in Scitacean.
+
+The object returned by :func:`scitacean.get_logger` is the only logger
+used by Scitacean. Scitacean does not configure it in any way.
+You are free to do so.
+"""
 
 import logging
 
 
-def get_logger():
-    return logging.getLogger("scitacean")
+def logger_name() -> str:
+    """Return the name of Scitacean's logger."""
+    return "scitacean"
+
+
+def get_logger() -> logging.Logger:
+    """Return the logger used by Scitacean."""
+    return logging.getLogger(logger_name())
