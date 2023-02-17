dac7a4778fe8d36df8d8bfb34fa47f7bbbf38dcf
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-02-17 14:48:14 +0100

    Improve table style

--- src/scitacean/_html_repr/__init__.py
+++ src/scitacean/_html_repr/__init__.py
@@ -25,8 +25,8 @@ def dataset_html_repr(dset: Dataset) -> str:
     return template.substitute(
         style_sheet=style_sheet,
         dataset_type=_format_dataset_type(dset.type),
-        number_of_files=dset.number_of_files,
-        size=_human_readable_size(dset.size),
+        file_info=_format_file_info(dset, archived=False),
+        archived_file_info=_format_file_info(dset, archived=True),
         main_rows=main_rows,
         detail_rows=detail_rows,
     )
@@ -39,39 +39,56 @@ def _format_field(dset: Dataset, field: Dataset.Field) -> str:
         if field.required(dset.type)
         else ""
     )
-    value = html.escape(str(getattr(dset, field.name)))
+    val = getattr(dset, field.name)
+    value = (
+        '<span class="cean-empty-field">None</span>'
+        if val is None
+        else html.escape(str(val))
+    )
     typ = _format_type(field.type)
     description = html.escape(field.description)
 
     template = resources.dataset_field_repr_template()
     return template.substitute(
-        name=name, required=required, type=typ, value=value, description=description
+        name=name, flag=required, type=typ, value=value, description=description
     )
 
 
-_EXCLUDED_FIELDS = (
+_EXCLUDED_FIELDS = {
     "type",
     "number_of_files",
+    "number_of_files_archived",
     "size",
-)
+    "packed_size",
+    "meta",  # TODO separate listing
+    "source_folder",  # TODO show with files
+}
 
-_DETAIL_FIELDS = (
-    "classification",
-    "creation_location",
-    "instrument_id",
-    "is_published",
+_MAIN_FIELDS = (
+    "pid",
+    "name",
+    "description",
+    "creation_time",
+    "proposal_id",
+    "sample_id",
+    "input_datasets",
 )
 
 
 def _get_field_specs(
     dset: Dataset, detail: bool
 ) -> Generator[Dataset.Field, None, None]:
-    it = filter(lambda f: f.name not in _EXCLUDED_FIELDS, Dataset.fields())
     if detail:
-        it = filter(lambda f: f.name in _DETAIL_FIELDS, it)
+        yield from sorted(
+            filter(
+                lambda f: f.name not in _MAIN_FIELDS and f.name not in _EXCLUDED_FIELDS,
+                Dataset.fields(),
+            ),
+            key=lambda f: not f.required(dset.type),
+        )
     else:
-        it = filter(lambda f: f.name not in _DETAIL_FIELDS, it)
-    yield from sorted(it, key=lambda f: not f.required(dset.type))
+        all_fields = {f.name: f for f in Dataset.fields()}
+        yield from (all_fields[name] for name in _MAIN_FIELDS)
 
 
 _TYPE_NAME = {
@@ -99,6 +116,23 @@ def _format_type(typ: Any) -> str:
 
 def _format_dataset_type(dataset_type: DatasetType) -> str:
     return "Raw" if dataset_type == DatasetType.RAW else "Derived"
+
+
+def _format_file_info(dset: Dataset, archived: bool) -> str:
+    if archived:
+        n_files = dset.number_of_files_archived
+        if n_files == 0:
+            # Don't show archived files if there aren't any.
+            # But always show regular files.
+            return ""
+        size = dset.packed_size
+        name = "archived files"
+    else:
+        n_files = dset.number_of_files
+        size = dset.size
+        name = "files"
+
+    return f"{n_files} {name} {_human_readable_size(size)}"
 
 
 def _human_readable_size(size_in_bytes: int) -> str:
