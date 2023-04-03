d7c38d35830f3a2fb01c207e7648935175391c7f
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-04-03 15:58:12 +0200

    Configure ssh server with seed data

--- tests/common/ssh_server.py
+++ tests/common/ssh_server.py
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/scitacean)
 
+import tempfile
 import time
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional, Union
 
 import fabric
 import fabric.config
@@ -16,6 +18,7 @@ from .docker import docker_compose
 _SSH_SERVER_DOCKER_CONFIG = (
     Path(__file__).resolve().parent / "docker-compose-ssh-server.yaml"
 )
+_SEED_DIR = Path(__file__).resolve().parent / "data/ssh_server_seed"
 _COMMAND_LINE_OPTION = "--ssh-tests"
 
 
@@ -46,7 +49,26 @@ def ssh_access():
 
 
 @pytest.fixture(scope="session")
-def ssh_fileserver(request, ssh_access):
+def ssh_config_dir(request) -> Optional[Path]:
+    if not request.config.getoption(_COMMAND_LINE_OPTION):
+        yield None
+        return
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        yield Path(tempdir)
+
+
+@pytest.fixture(scope="session")
+def ssh_data_dir(ssh_config_dir) -> Optional[Path]:
+    if ssh_config_dir is None:
+        yield None
+        return
+
+    yield ssh_config_dir / "data"
+
+
+@pytest.fixture(scope="session")
+def ssh_fileserver(request, ssh_access, ssh_config_dir):
     """Spin up an SSH server.
 
     Does nothing unless the --ssh-tests command line option is set.
@@ -55,9 +77,34 @@ def ssh_fileserver(request, ssh_access):
         yield False
         return
 
-    with docker_compose(_SSH_SERVER_DOCKER_CONFIG):
+    config_file = configure(ssh_config_dir)
+    with docker_compose(config_file):
         wait_until_backend_is_live(ssh_access, max_time=20, n_tries=20)
         yield True
+
+
+def configure(target_dir: Union[Path, str]) -> Path:
+    """Generate a config file for docker compose and symlink seed data."""
+    target_dir = Path(target_dir)
+    target_seed_dir = target_dir / "data" / "seed"
+    target_seed_dir.parent.mkdir()
+    target_seed_dir.symlink_to(_SEED_DIR)
+    # (target_dir/"data"/"asd").mkdir()
+
+    with open(_SSH_SERVER_DOCKER_CONFIG, "r") as f:
+        config = yaml.safe_load(f)
+    service = config["services"]["scitacean-test-ssh-server"]
+
+    # Mount the base data dir. This gives the user write access.
+    service.setdefault("volumes", []).append(f"{target_dir / 'data'}:/data")
+    # Mount the initial data via the symlink created above.
+    # This gives the user read access.
+    service.setdefault("volumes", []).append(f"{target_seed_dir}:/data/seed")
+
+    target = target_dir / _SSH_SERVER_DOCKER_CONFIG.name
+    with open(target, "w") as f:
+        yaml.dump(config, f)
+    return target
 
 
 def can_connect(ssh_access: SSHAccess) -> bool:
