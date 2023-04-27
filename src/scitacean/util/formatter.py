d0c22c1e12097077491354c0d7c60a793c466270
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-04-27 10:49:32 +0200

    Support uid in DatasetPathFormatter

--- src/scitacean/util/formatter.py
+++ src/scitacean/util/formatter.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/scitacean)
 """String-formatting tools."""
 
-
 from string import Formatter
 from typing import Any, Iterator, Optional, Tuple
 
@@ -18,13 +17,13 @@ class DatasetPathFormatter(Formatter):
     .. code-block:: python
 
         formatter = DatasetPathFormatter()
-        formatter.format("{owner}-{pid.pid}", dset)
+        formatter.format("{owner}-{pid.pid}-{uid}", dset=dset, uid=uid)
         # is equivalent to (up to escaping)
-        "{0.owner}-{0.pid.pid}".format(dset)
+        "{dset.owner}-{dset.pid.pid}-{uid}".format(dset=dset, uid=uid)
 
     This means that all format fields are transformed to access attributes
-    for the first argument of ``format``. ``format`` thus supports only a
-    single argument.
+    of the keyword argument ``dset``.
+    Except for ``"uid"`` which is preserved to allow for a dedicated ``uid`` keyword.
 
     In addition, all field values are escaped as filesystem paths
     using :func:`scitacean.filesystem.escape_path`:
@@ -32,9 +31,9 @@ class DatasetPathFormatter(Formatter):
     .. code-block:: python
 
         formatter = DatasetPathFormatter()
-        formatter.format("a string {owner}", dset)
+        formatter.format("a string {owner}", dset=dset)
         # is equivalent to
-        "a string {}".format(escape_path("{0.owner}".format(dset)))
+        "a string {}".format(escape_path("{dset.owner}".format(dset=dset)))
 
     Note that only formatted fields are escaped, not the result as a whole.
     """
@@ -43,7 +42,11 @@ class DatasetPathFormatter(Formatter):
         self, format_string: str
     ) -> Iterator[Tuple[str, Optional[str], Optional[str], Optional[str]]]:
         def add0(field_name: Optional[str]) -> Optional[str]:
-            return "0." + field_name if isinstance(field_name, str) else None
+            if field_name == "uid":
+                return field_name
+            if isinstance(field_name, str):
+                return "dset." + field_name
+            return None
 
         return map(
             lambda t: (t[0], add0(t[1]), t[2], t[3]), super().parse(format_string)
