5b9ae533a9877eb7c15b88eb72b8faaf0f7890c7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-04-27 15:12:38 +0200

    Make pid mutable but disallow it in upload

--- src/scitacean/_dataset_fields.py
+++ src/scitacean/_dataset_fields.py
@@ -378,7 +378,7 @@ class DatasetFields:
         Field(
             name="pid",
             description="Persistent identifier for datasets.",
-            read_only=True,
+            read_only=False,
             required_by_derived=False,
             required_by_raw=False,
             type=PID,
