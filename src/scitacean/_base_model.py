e9344ffe8404a13bf606b6d26047e58d370d4558
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-05-01 16:50:41 +0200

    Add BaseUserModel

--- src/scitacean/_base_model.py
+++ src/scitacean/_base_model.py
@@ -16,7 +16,7 @@ except ImportError:
         _Enum,
     )
 
-import re
+import dataclasses
 from typing import Any, Dict, Optional, Type, TypeVar
 
 import pydantic
@@ -37,6 +37,8 @@ class DatasetType(*_DatasetTypeBases):
 
 
 class BaseModel(pydantic.BaseModel):
+    """Base class for Pydantic models for communication with SciCat."""
+
     class Config:
         extra = pydantic.Extra.forbid
         json_encoders = {
@@ -44,15 +46,71 @@ class BaseModel(pydantic.BaseModel):
             RemotePath: lambda v: v.posix,
         }
 
-    def camel_case_dict(self, *args, **kwargs) -> Dict[str, Any]:
-        """Return a dict representation of the model with camelCase names.
 
-        Arguments are forwarded to ``pydantic.BaseModel.dict()``.
-        """
+class BaseUserModel:
+    """Base class for user models.
+
+    Child classes must be dataclasses.
+    """
+
+    @classmethod
+    def _download_model_dict(cls, download_model: Any) -> Dict[str, Any]:
         return {
-            _snake_case_to_camel_case(key): value
-            for key, value in self.dict(*args, **kwargs)
+            field.name: getattr(download_model, _model_field_name_of(field.name))
+            for field in dataclasses.fields(cls)
         }
+
+    def _upload_model_dict(self) -> Dict[str, Any]:
+        _check_ready_for_upload(self)
+        return {
+            _model_field_name_of(field.name): getattr(self, field.name)
+            for field in dataclasses.fields(self)
+            if not field.name.startswith("_")
+        }
+
+
+def construct(
+    model: Type[ModelType],
+    *,
+    _strict_validation: bool = True,
+    **fields: Any,
+) -> ModelType:
+    """Instantiate a SciCat model.
+
+    Warning
+    -------
+    If the model is created without validation, no fields will be converted
+    to their proper type but will simply be whatever arguments are passed.
+
+    A warning will be emitted in this case.
+
+    Parameters
+    ----------
+    model:
+        Class of the model to create.
+    _strict_validation:
+        If ``True``, the model must pass validation.
+        If ``False``, a model is still returned if validation fails.
+    fields:
+        Field values to pass to the model initializer.
+
+    Returns
+    -------
+    :
+        An initialized model.
+    """
+    try:
+        return model(**fields)
+    except pydantic.ValidationError as e:
+        if _strict_validation:
+            raise
+        get_logger().warning(
+            "Validation of metadata failed: %s\n"
+            "The returned object may be incomplete or broken. "
+            "In particular, some fields may not have the correct type",
+            str(e),
+        )
+        return model.construct(**fields)
 
 
 def validate_emails(value: Optional[str]) -> Optional[str]:
@@ -75,68 +133,25 @@ def validate_orcid(value: Optional[str]) -> Optional[str]:
     )
 
 
-def construct(
-    model: Type[ModelType],
-    *,
-    _strict_validation: bool = True,
-    _from_camel_case: bool = False,
-    **fields: Any,
-) -> ModelType:
-    """Instantiate a model.
+def _model_field_name_of(name: str) -> str:
+    """Convert a user model field name to a SciCat model field name.
 
-    Warning
-    -------
-    If the model is created without validation, no fields will be converted
-    to their proper type but will simply be whatever arguments are passed.
-
-    A warning will be emitted in this case.
-
-    Parameters
-    ----------
-    model:
-        Class of the model to create.
-    _strict_validation:
-        If ``True``, the model must pass validation.
-        If ``False``, a model is still returned if validation fails.
-    _from_camel_case:
-        If ``True``, file names are converted from camelCase to snake_case.
-    fields:
-        Field values to pass to the model initializer.
-
-    Returns
-    -------
-    :
-        An initialized model.
+    Converts snake_case to camelCase and strips leading underscores.
     """
-    if _from_camel_case:
-        fields = {
-            _camel_case_to_snake_case(key): value for key, value in fields.items()
-        }
-
-    try:
-        return model(**fields)
-    except pydantic.ValidationError as e:
-        if _strict_validation:
-            raise
-        get_logger().warning(
-            "Validation of metadata failed: %s\n"
-            "The returned object may be incomplete or broken. "
-            "In particular, some fields may not have the correct type",
-            str(e),
-        )
-        return model.construct(**fields)
-
-
-def _snake_case_to_camel_case(string: str) -> str:
-    """Convert a string from snake_case to camelCase."""
-    first, *remainder = string.split("_")
+    first, *remainder = name.lstrip("_").split("_")
     return first + "".join(word.capitalize() for word in remainder)
 
 
-def _camel_case_to_snake_case(string: str) -> str:
-    """Convert a string from camelCase to snake_case."""
-
-    def repl(match):
-        return "_" + match[1].lower()
-
-    return re.sub(r"([A-Z])", repl, string)
+def _check_ready_for_upload(user_model: BaseUserModel) -> None:
+    download_only = {
+        field.name: value
+        for field in dataclasses.fields(user_model)
+        if field.name.startswith("_")
+        and (value := getattr(user_model, field.name)) is not None
+    }
+    if download_only:
+        raise ValueError(
+            f"These fields of {user_model.__class__.__name__} "
+            "must not be set during upload:\n"
+            + "\n".join(f"  {name} = {value}" for name, value in download_only.items())
+        )
