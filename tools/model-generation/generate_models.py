fa2f7d4bf7336e5ac4c3a36353e8987e00dd1bc1
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-05-02 14:46:48 +0200

    Clean up old generator code

--- tools/model-generation/generate_models.py
+++ tools/model-generation/generate_models.py
@@ -1,23 +1,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/scitacean)
 """Generate pydantic models from specifications."""
-import dataclasses
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict
 
 from jinja2 import Environment, FileSystemLoader, Template
 from spec import Spec
 from templates import BANNER
 
 
-@dataclasses.dataclass
-class _UpDownSpec:
-    upload: Optional[Spec]
-    download: Spec
-
-
-def quote(x):
-    return f'"{x}"'
+def quote(s: str) -> str:
+    if '"' in s:
+        return f"'{s}'"
+    return f'"{s}"'
 
 
 def _template() -> Template:
