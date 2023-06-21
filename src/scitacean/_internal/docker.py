b8dd8ee3aeddda66b09cd67b116c8048fdf893a8
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-06-21 08:54:58 +0200

    Skip failed lines in JSON parser

--- src/scitacean/_internal/docker.py
+++ src/scitacean/_internal/docker.py
@@ -7,12 +7,12 @@ Primarily meant for testing.
 import json
 import os
 import subprocess
-from typing import Union
+from typing import Any, Union
 
 _PathLike = Union[str, os.PathLike]
 
 
-def docker_compose_up(config_file: _PathLike, *services: str):
+def docker_compose_up(config_file: _PathLike, *services: str) -> None:
     subprocess.check_call(
         [
             "docker",
@@ -27,16 +27,26 @@ def docker_compose_up(config_file: _PathLike, *services: str):
     )
 
 
-def docker_compose_down(config_file: _PathLike):
+def docker_compose_down(config_file: _PathLike) -> None:
     subprocess.check_call(
         ["docker", "compose", "--file", os.fspath(config_file), "down", "--volumes"]
     )
 
 
-def docker_compose_run(config_file: _PathLike, service: str, *cmd: str):
+def docker_compose_run(config_file: _PathLike, service: str, *cmd: str) -> None:
     subprocess.check_call(
         ["docker", "compose", "--file", os.fspath(config_file), "run", service, *cmd]
     )
+
+
+def _try_parse_json(s: str) -> Any:
+    # The precise output of `docker ps` can vary.
+    # So parse it leniently; if a line isn't valid JSON, it surely does not
+    # contain a container name.
+    try:
+        return json.loads(s)
+    except json.JSONDecodeError:
+        return None
 
 
 def container_is_running(name: str) -> bool:
@@ -44,7 +54,7 @@ def container_is_running(name: str) -> bool:
         ["docker", "ps", "--format=json"], stdout=subprocess.PIPE, check=True
     )
     lines = ps.stdout.decode(encoding="utf-8").split("\n")
-    containers = [json.loads(line) for line in lines if line]
+    containers = [x for line in lines if (x := _try_parse_json(line))]
     for container in containers:
         names = container["Names"]
         if (isinstance(names, str) and names == name) or name in names:
