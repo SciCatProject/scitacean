c263ba07172dae58bd2575f664c63cb8845cb6e0
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-08-08 11:51:37 +0200

    Do not subscript type

--- src/scitacean/_internal/dataclass_wrapper.py
+++ src/scitacean/_internal/dataclass_wrapper.py
@@ -21,7 +21,7 @@ except ImportError:
         order_default: bool = False,
         kw_only_default: bool = False,
         frozen_default: bool = False,
-        field_specifiers: Tuple[Union[type[Any], Callable[..., Any]], ...] = (),
+        field_specifiers: Tuple[Union[Type[Any], Callable[..., Any]], ...] = (),
         **kwargs: Any,
     ) -> Callable[[T], T]:
         def impl(f: F) -> F:
