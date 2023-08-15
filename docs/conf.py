4ffd4dc978c138f00390892f5e6088047e6fb032
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-08-15 16:35:35 +0200

    Fix intersphinx mapping for paramiko

--- docs/conf.py
+++ docs/conf.py
@@ -39,10 +39,10 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    "fabric": ("https://docs.fabfile.org/en/stable", None),
+    "fabric": ("https://docs.fabfile.org/en/latest", None),
     "hypothesis": ("https://hypothesis.readthedocs.io/en/latest/", None),
     "python": ("https://docs.python.org/3", None),
-    "paramiko": ("https://docs.paramiko.org/en/stable", None),
+    "paramiko": ("https://docs.paramiko.org/en/latest", None),
 }
 
 # autodocs includes everything, even irrelevant API internals. autosummary
