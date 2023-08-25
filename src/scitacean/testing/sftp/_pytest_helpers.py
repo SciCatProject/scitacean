a6d2fd24f1a58e96fa28836b7d4bfd2d39fff91a
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-08-25 13:50:31 +0200

    Copy ssh transfer to sftp transfer

---
+++ src/scitacean/testing/sftp/_pytest_helpers.py
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/scitacean)
+
+from typing import Optional
+
+import pytest
+
+_COMMAND_LINE_OPTION: Optional[str] = None
+
+
+def add_pytest_option(parser: pytest.Parser, option: str = "--sftp-tests") -> None:
+    """Add a command-line option to pytest to toggle SFTP tests.
+
+    Parameters
+    ----------
+    parser:
+        Pytest's command-line argument parser.
+    option:
+        Name of the command-line option.
+    """
+    parser.addoption(
+        option,
+        action="store_true",
+        default=False,
+        help="Select whether to run tests with an SFTP fileserver",
+    )
+    global _COMMAND_LINE_OPTION
+    _COMMAND_LINE_OPTION = option
+
+
+def skip_if_not_sftp(request: pytest.FixtureRequest) -> None:
+    """Mark the current test to be skipped if SFTP tests are disabled."""
+    if not sftp_enabled(request):
+        pytest.skip(
+            "Tests against an SFTP file server are disabled, "
+            f"use {_COMMAND_LINE_OPTION} to enable them"
+        )
+
+
+def sftp_enabled(request: pytest.FixtureRequest) -> bool:
+    """Return True if SFTP tests are enabled."""
+    return _COMMAND_LINE_OPTION is not None and bool(
+        request.config.getoption(_COMMAND_LINE_OPTION)
+    )
