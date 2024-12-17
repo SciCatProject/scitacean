81dc0c1b0df2642ccf644c0c187c2ca35680acd4
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-12-17 10:25:46 +0100

    Fix more tests

--- tests/transfer/sftp_test.py
+++ tests/transfer/sftp_test.py
@@ -402,6 +402,7 @@ def test_client_with_sftp(
         contact_email="p.stibbons@uu.am",
         creation_location="UU",
         creation_time=datetime(2023, 6, 23, 10, 0, 0, tzinfo=timezone.utc),
+        name="Secret Thaum Storage",
         owner="PonderStibbons",
         owner_group="uu",
         principal_investigator="MustrumRidcully",
