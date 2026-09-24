c3dbf5ab3a662dabadcfbc056e718038d26fdb69
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2026-09-24 15:15:27 +0200

    Fix url_concat

--- src/scitacean/_internal/url.py
+++ src/scitacean/_internal/url.py
@@ -8,7 +8,7 @@ from urllib.parse import urlsplit
 def url_concat(a: str, b: str) -> str:
     """Combine two pieces or a URL without handling absolute paths as in urljoin."""
     a = a if a.endswith("/") else (a + "/")
-    b = b[1:] if b.endswith("/") else b
+    b = b.removeprefix("/")
     return a + b
 
 
